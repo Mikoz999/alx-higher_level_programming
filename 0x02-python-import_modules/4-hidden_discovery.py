d01f7803d742809116004c893f47c7a22aee1029
Mikoz999 <mikozotieno@gmail.com>
2023-08-15 20:33:45 -0700

    List and Print Public Names in hidden_4 Module
    This commit introduces a script that imports the hidden_4 module and lists all the names defined within it. The script filters out and prints the public names (those not starting with double underscores) using a loop and the dir() function. This provides visibility into the public components of the module.

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import dis
+
+def print_names(module):
+    for name in dir(module):
+        if not name.startswith("__"):
+            print(name)
+
+if __name__ == "__main__":
+    import hidden_4
+    print_names(hidden_4)
